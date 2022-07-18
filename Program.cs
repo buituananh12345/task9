4d69eed5c3f9dff61e3de4100f7963b2efca44b0
buituananh12345 <103102389+buituananh12345@users.noreply.github.com>
2022-07-18 17:12:30 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿public abstract class GeomettriObject
+{
+    protected string color;
+    protected double weight;
+    // Default construct
+    protected GeotricOblect()
+    {
+        color = "white";
+        weight = 1.0;
+    }
+    // Contruct a geometric object
+    protected GeomettriObject(string color, double weight)
+    {
+        this.color = color;
+        this.weight = weight;
+    }
+    //properties
+    publicstring PColor
+    {
+        get { return color; }
+        set { color = value; }
+    }
+    public double PWeight
+    {
+        get { return weight; }
+        set { weight = value; }
+    }
+    // Abtract method 
+    public abstract double findArea();
+    // Abtract method
+    public abstract double findPerimeter();
+}
